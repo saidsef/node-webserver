dba9c1f2d17d463ad4789d2b02c21e5001a2d959
Said Sef <saidsef@gmail.com>
2017-12-26 21:07:05 +0000

    added build script

---
+++ build.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+BUILD_ID=$1
+
+info() {
+  echo """
+    This is a containerised Node Application Webserver
+  """
+}
+
+cleanup() {
+  echo "Remove dorment containers"
+  if [ ! -z "$(docker ps -a -q | head -n 1)" ]; then
+    docker stop $(docker ps -a -q)
+    docker rm -f $(docker ps -a -q)
+  else
+    echo "There is no dorment containers"
+  fi
+}
+
+delete() {
+  echo "Remove dorment images"
+  if [ ! -z "$(docker images -a -q | head -n 1)" ]; then
+    docker rmi -f $(docker images -a -q) || true
+  else
+    echo "There is no dorment images"
+  fi
+}
+
+build() {
+  echo "Building image"
+  docker build --build-arg "BUILD_ID=${BUILD_ID}" -t saidsef/node-webserver .
+  docker tag saidsef/node-webserver saidsef/node-webserver:build-${BUILD_ID}
+}
+
+push() {
+  echo "Pushing image to docker hub"
+  docker push saidsef/node-webserver
+  echo $?
+}
+
+main() {
+  info
+  cleanup
+  build
+  push
+  cleanup
+  delete
+}
+
+main
