7598b4ffe5a73c46d9646784cc3162d8e56fbddd
Said Sef <saidsef@gmail.com>
2018-01-19 00:22:24 +0000

    added file check for build_id

--- app/index.js
+++ app/index.js
@@ -2,17 +2,18 @@
 
 const express = require('express');
 const logging = require("morgan");
-const fs = require("fs");
+const fs      = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 80;
 
-app.use(logging("dev"));
+app.use(logging("combined"));
 
 app.get("*", (req, res, next) => {
+  const fileName = "build_id.txt";
   let randomNumber = parseInt(String(parseFloat((Math.random() * 100)).toFixed(2)),10);
-  let buildID = fs.readFileSync("build_id.txt", "utf-8").trim();
-  res.json({ "message": "Hello World!", "random number": randomNumber, "build": buildID });
+  let buildID = (fs.existsSync(fileName)) ? fs.readFileSync(fileName, "utf-8").trim() : "NOFILE";
+  res.json({ "message": "Hello World!", "random_number": randomNumber, "build": buildID });
 });
 
 app.listen(PORT);
