a07f2da1ea2159a372170fc4a64843a50a5c4440
Said Sef <saidsef@gmail.com>
2021-04-15 10:33:25 +0100

    syntax fixes

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@ info() {
 
 cleanup() {
   echo "Remove dorment containers"
-  if [ ! -z "$(docker ps -a -q | head -n 1)" ]; then
-    docker stop $(docker ps -a -q)
-    docker rm -f $(docker ps -a -q)
+  if [ -n "$(docker ps -a -q | head -n 1)" ]; then
+    docker stop "$(docker ps -a -q)" || true
+    docker rm -f "$(docker ps -a -q)" || true
   else
     echo "There is no dorment containers"
   fi
@@ -20,8 +20,8 @@ cleanup() {
 
 delete() {
   echo "Remove dorment images"
-  if [ ! -z "$(docker images -a -q | head -n 1)" ]; then
-    docker rmi -f $(docker images -a -q) || true
+  if [ -n "$(docker images -a -q | head -n 1)" ]; then
+    docker rmi -f "$(docker images -a -q)" || true
   else
     echo "There is no dorment images"
   fi
@@ -32,7 +32,7 @@ build() {
   docker build --build-arg "BUILD_ID=${BUILD_ID}" -t saidsef/node-webserver .
   docker build --build-arg "BUILD_ID=${BUILD_ID}" -t saidsef/node-webserver:arm64 . -f Dockerfile.arm64
   docker build --build-arg "BUILD_ID=${BUILD_ID}" -t saidsef/node-webserver:armhf . -f Dockerfile.armhf
-  docker tag saidsef/node-webserver saidsef/node-webserver:build-${BUILD_ID}
+  docker tag saidsef/node-webserver "saidsef/node-webserver:build-${BUILD_ID}"
 }
 
 buildx() {
