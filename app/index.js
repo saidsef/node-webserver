71fd89ea9caaf86f9a41728edc96fe3fe3d3ec41
Said Sef <6002187+saidsef@users.noreply.github.com>
2024-05-14 20:14:53 +0100

    feat: replace moragn with http-pino (#101)
    * chore: update dep js libs
    
    * feat: replace morgan with http-pino
    
    * chore: updated dev container
    
    * chore: add dev cnotainer updates

--- app/index.js
+++ app/index.js
@@ -1,20 +1,20 @@
 'use strict';
 
 const express     = require('express');
-const logging     = require("morgan");
+const logging     = require('pino-http');
 const compression = require('compression');
 const helmet      = require('helmet');
 const crypto      = require('crypto');
-const cors        = require('cors');
 
 const app  = express();
 const http = require('http').createServer(app);
 const PORT = process.env.PORT || 8080;
 
+app.disable('x-powered-by');
 app.enable('trust proxy');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json()); // support json encoded bodies
-app.use(logging("combined"));
+app.use(logging({ level: process.env.LOG_LEVEL || 'info' }));
 app.use(helmet({
   contentSecurityPolicy: {
     directives: {
@@ -22,7 +22,7 @@ app.use(helmet({
       defaultSrc: ["'self'"],
       formAction: ["'none'"],
       frameAncestors: ["'none'"],
-      imgSrc: ["'self"],
+      imgSrc: ["'self'"],
       objectSrc: ["'self'"],
       sandbox: ['allow-forms', 'allow-scripts'],
       scriptSrc: ["'none'"],
@@ -33,9 +33,7 @@ app.use(helmet({
   referrerPolicy: {policy: 'same-origin'},
 }));
 
-app.use(cors());
 app.use(compression());
-app.disable('x-powered-by');
 
 app.get('/healthz', (req, res, next) => {
   res.set({'X-Robots-Tag' : 'noindex, nofollow, noarchive'});
@@ -48,14 +46,14 @@ app.get("*", (req, res, next) => {
   const number = crypto.randomInt(10000);
   const sha1 = crypto.createHash('sha1').update(crypto.randomBytes(20)).digest('hex');
   res.json({
-    "message": "Hello World!",
-    "ip": ip,
-    "ips": ips,
-    "random_number": number,
-    "random_sha1": sha1,
-    "build": buildID,
-    "request": headers,
-    "environment_vars": process.env
+    message: "Hello World!",
+    ip: ip,
+    ips: ips,
+    random_number: number,
+    random_sha1: sha1,
+    build: buildID,
+    request: headers,
+    environment_vars: process.env
   });
 });
 
