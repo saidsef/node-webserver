7acfe5c6df88e42c80e0f8e7b106324d808bd1b4
Said Sef <saidsef@gmail.com>
2024-12-25 10:05:32 +0000

    feat: refactor routes to be more modular

---
+++ app/routes.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const crypto = require('crypto');
+const router = express.Router();
+
+router.get('/healthz', (req, res) => {
+  res.set({'X-Robots-Tag': 'noindex, nofollow, noarchive'});
+  res.json({'status': 'healthy'});
+});
+
+router.get('*', (req, res) => {
+  const { headers, ip, ips } = req;
+  const buildID = process.env.BUILD_ID;
+  const number = crypto.randomInt(10000);
+  const sha1 = crypto.createHash('sha1').update(crypto.randomBytes(20)).digest('hex');
+  res.json({
+    message: "Hello World!",
+    ip: ip,
+    ips: ips,
+    random_number: number,
+    random_sha1: sha1,
+    build: buildID,
+    request: headers,
+    environment_vars: process.env
+  });
+});
+
+module.exports = router;
